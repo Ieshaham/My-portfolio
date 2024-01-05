4d2a20bebf44bdf8aad67e39151fca8142ff8ebe
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2024-01-05 16:40:10 +0000

    Initialized project, set up components, project functioning correctly

---
+++ iesha-portfolio/src/App.js
@@ -0,0 +1,25 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
