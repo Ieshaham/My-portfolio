fdac3b873e96df37c13465c0d051fe4841548d0e
Ieshaham <131932350+Ieshaham@users.noreply.github.com>
2024-02-01 00:40:47 +0000

    project started

--- iesha-portfolio/src/components/Projects.jsx
+++ iesha-portfolio/src/components/Projects.jsx
@@ -1,61 +1,92 @@
 import React, { useEffect, useState } from 'react';
-import { faHtml5,faCss3Alt,faJs,faReact,faNodeJs,faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3, faJs, faReact, faNodeJs, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 
 const Projects = () => {
   const [isFirstProjectVisible, setFirstProjectVisible] = useState(false);
   const [isSecondProjectVisible, setSecondProjectVisible] = useState(false);
-
+  const [isThirdProjectVisible, setThirdProjectVisible] = useState(false);
+  // const [isFourthProjectVisible, setFourthProjectVisible] = useState(false);
+  
   useEffect(() => {
-    // Use setTimeout to delay the appearance of the second project
-    const timeout = setTimeout(() => {
+    const timeout1 = setTimeout(() => {
       setFirstProjectVisible(true);
-    }, 500); // Adjust the delay time as needed
+    }, 500);
 
-    // Clean up the timeout on component unmount
-    return () => clearTimeout(timeout);
-  }, []);
-
-  useEffect(() => {
-    // Use setTimeout to delay the appearance of the second project
-    const timeout = setTimeout(() => {
+    const timeout2 = setTimeout(() => {
       setSecondProjectVisible(true);
-    }, 1000); // Adjust the delay time as needed
+    }, 1000);
 
-    // Clean up the timeout on component unmount
-    return () => clearTimeout(timeout);
+    const timeout3 = setTimeout(() => {
+      setThirdProjectVisible(true);
+    }, 1000);
+
+    // const timeout4 = setTimeout(() => {
+    //   setFourthProjectVisible(true);
+    // }, 1000);
+
+    // Clean up timeouts on component unmount
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+      // clearTimeout(timeout4);
+    };
   }, []);
 
   return (
-    
-      <div className='project1container'>
-        <div className={`image-grid ${isFirstProjectVisible ? 'show-first' : ''}`}>
-          <div className={`first-project ${isFirstProjectVisible ? 'fade-in' : ''}`}>
-            <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image1' />
-            <div className='project-details'>
-              <p className='first-title'><strong>Voyage Hawk</strong></p>
-              <p className='voyage-hawk'>
-                A full-stack creation designed to revolutionize travel planning. This application enables users to effortlessly craft travel itineraries and discover new destinations. With a sleek interface and dynamic functionality, Voyage Hawk offers a user-friendly platform for seamless travel exploration.
-              </p>
-              <div className='project1buttons'>
-                <p className='applications-project1'>
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+    <div className={`col ${isFirstProjectVisible ? 'visible' : 'hidden'}`}>
+    <div class="card">
+    <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image1' />
+      <div class="card-body">
+        <h5 class="card-title">Voyage Hawk</h5>
+        <p class="card-text">A full-stack creation designed to revolutionize travel planning. 
+        This application enables users to effortlessly craft travel itineraries and discover new destinations. 
+        With a sleek interface and dynamic functionality, Voyage Hawk offers a user-friendly platform for seamless travel exploration.</p>
+        <div className='project1buttons'>
+          <div className="applications-project1">
                   <div className='application'> <FontAwesomeIcon icon={faHtml5} />  </div>  
-                  <div className='application'><FontAwesomeIcon icon={faJs} /></div>
-                  <div className='application'><FontAwesomeIcon icon={faReact} /></div>
-                  <div className='application'><FontAwesomeIcon icon={faGoogle} /></div>
-                  <div className='application'><FontAwesomeIcon icon={faNodeJs} /></div>
-                </p>
-              </div>
-          </div>
-        </div>
-        <div className={`second-project ${isSecondProjectVisible ? 'fade-in' : ''}`}>
-          <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image2' />
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-        </div>
+                  <div className='application'><FontAwesomeIcon icon={faCss3} /></div>
+                   <div className='application'><FontAwesomeIcon icon={faJs} /></div>
+                   <div className='application'><FontAwesomeIcon icon={faReact} /></div>
+                   <div className='application'><FontAwesomeIcon icon={faGoogle} /></div>
+                   <div className='application'><FontAwesomeIcon icon={faNodeJs} /></div>
+                 </div>
+               </div>
       </div>
     </div>
-  );
-}
+  </div>
+  <div className={`col ${isSecondProjectVisible ? 'visible' : 'hidden'}`}>
+    <div class="card">
+    <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image1' />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className={`col ${isThirdProjectVisible ? 'visible' : 'hidden'}`}>
+    <div class="card">
+    <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image1' />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  {/* <div className={`col ${isFourthProjectVisible ? 'visible' : 'hidden'}`}>
+    <div class="card">
+    <img src={process.env.PUBLIC_URL + '/project1.png'} alt="Your Image" className='image1' />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div> */}
+</div>
+    
+    );
+}    
+    export default Projects;
 
-export default Projects;
