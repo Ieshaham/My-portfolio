0683e31a4580815d8b81da8891b8e499437318ff
Iesha Ham <131932350+Ieshaham@users.noreply.github.com>
2025-03-14 20:51:26 +0000

    ballon componenet added

--- iesha-portfolio/src/components/Home.jsx
+++ iesha-portfolio/src/components/Home.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import Contact from './Contact';
 import About from './About';
-import Projects from './Projects'
+import Projects from './Projects';
 import ScrollToTopButton from './ScrollToTop';
+import FloatingBalloons from './FloatingBalloons';
 
 const Home = () => {
   return (
     <div className='home-container'>
-   <navbar />
+      <navbar />
+
+      {/* Add the FloatingBalloons component */}
+      <FloatingBalloons />
 
       <div className="page-content">
         <h4 id="scrollspyHeading1"></h4>
         <div className='page-wording'>
-         <i> <h2 className='name'>Hi, I'm Iesha. </h2></i>
+          <img className='name_icon' src='name_icon.png' alt="Name Icon" />
           <p className='slogan'>
             A software engineer that<br />
             specializes in transforming ideas<br />
             into innovative solutions.
           </p>
-        
         </div>
 
         <h4 id="scrollspyHeading2"></h4>
